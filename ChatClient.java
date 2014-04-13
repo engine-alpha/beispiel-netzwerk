5e8ba70ceeb96e47b0579505ae0a0583f38df80b
Niklas Keller <me@kelunik.com>
2014-04-13 18:13:46 +0200

    simplified

--- ChatClient.java
+++ ChatClient.java
@@ -9,57 +9,38 @@ import ea.*;
  * @author Niklas Keller <me@kelunik.com>
  * @version 13. April 2014
  */
-public class ChatClient {
-	private Thread messageThread;
-	
-    public ChatClient(final String name) {    	
-        ServerDiscovery.startDiscovery(new ConnectListener() {
-            public void onConnect(String ip) {
-            	final Client client = new Client(ip, 15135) {
-                    public void empfangeString(String message) {
-                    	System.out.printf("[%s] %s\n", new Date().toString(), message);
-                    }
-                };
-                
-                System.out.printf("[%s] %s: %s\n", new Date().toString(), "Bot", "Server verbunden: " + ip);
-                
-                messageThread = new Thread() {
-                	public void run() {
-                		while(!isInterrupted()) {
-                			try {
-                				client.sendeString(name + ": " + readMessage());
-                			} catch(Exception e) {
-                				e.printStackTrace();
-                			}
-                		}
-                	}
-                };
-                
-                messageThread.start();
+public class ChatClient implements ServerGefundenReagierbar {
+    private String name;
+    
+    public ChatClient(final String name) {      
+        this.name = name;
+        ServerSuche.start(this);
+    }
+
+    public void serverGefunden(String ip) {
+        final Client client = new Client(ip, 15135) {
+            public void empfangeString(String message) {
+                System.out.println("[" + new Date().toString() + "] " + message);
             }
-        });
+        };
+        
+        System.out.println("[" + new Date().toString() + "] " + "Bot: Server verbunden @ " + ip);
+
+        while(true) {
+            try {
+                client.sendeString(name + ": " + readMessage());
+            } catch(Exception e) {
+                e.printStackTrace();
+            }
+        }
     }
-    
-    public void kill() {
-    	if(messageThread == null) {
-    		return;
-    	}
-    	
-    	messageThread.interrupt();
-    	
-    	try {
-			messageThread.join();
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-    }
-    
+
     // http://stackoverflow.com/questions/4203646/system-console-returns-null
     public String readMessage() throws IOException {
-    	if (System.console() != null) {
+        if (System.console() != null) {
             return System.console().readLine();
         }
-    	
+
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         return reader.readLine();
     }
--- ChatServer.java
+++ ChatServer.java
@@ -9,57 +9,31 @@ import ea.*;
  * @author Niklas Keller <me@kelunik.com>
  * @version 13. April 2014
  */
-public class ChatServer {
+public class ChatServer implements VerbindungHergestelltReagierbar {
 	private final Server server;
-	private final Thread messageThread;
     
     public ChatServer(final String name) {
     	this.server = new Server(15135) {
         	public void empfangeString(String message) {
-        		System.out.printf("[%s] %s\n", new Date().toString(), message);
+        		System.out.println("[" + new Date().toString() + "] " + message);
         	}
         };
         
         this.server.setBroadcast(true);
+        this.server.setVerbindungHergestelltReagierbar(this);
+        this.server.netzwerkSichtbarkeit(true);
         
-        this.server.setOnConnectListener(new ConnectListener() {
-        	public void onConnect(String ip) {
-                System.out.printf("[%s] %s: %s\n", new Date().toString(), "Bot", "Client verbunden: " + ip);
-            }
-        });
-        
-        this.messageThread = new Thread() {
-        	public void run() {
-        		while(!isInterrupted()) {
-        			try {
-        				server.sendeString(name + ": " + readMessage());
-        			} catch(Exception e) {
-        				e.printStackTrace();
-        			}
-        		}
-        	}
-        };
-        
-        this.messageThread.start();
-        this.setNetworkVisible(true);
-    }
-    
-    public void setNetworkVisible(boolean visible) {
-    	server.netzwerkSichtbarkeit(visible);
-    }
-    
-    public void kill() {
-    	setNetworkVisible(false);
-    	
-    	messageThread.interrupt();
-    	
-    	try {
-			messageThread.join();
-		} catch (InterruptedException e) {
-			e.printStackTrace();
+        while(true) {
+			try {
+				server.sendeString(name + ": " + readMessage());
+			} catch(Exception e) {
+				e.printStackTrace();
+			}
 		}
-    	
-    	server.beendeVerbindung();
+    }
+    
+    public void verbindungHergestellt(String ip) {
+        System.out.println("[" + new Date().toString() + "] Bot: Client verbunden mit IP " + ip);
     }
     
     // http://stackoverflow.com/questions/4203646/system-console-returns-null
